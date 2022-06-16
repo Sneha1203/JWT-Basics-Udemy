4039f9484127a13dd5c1fdd19ce2a4bd1c048d71
Sneha1203 <78474043+Sneha1203@users.noreply.github.com>
2022-06-16 13:13:30 +0530

    completed

--- controllers/main.js
+++ controllers/main.js
@@ -1,10 +1,21 @@
+const {BadRequestError} = require('../errors')
 const login = async (request, response) => {
-    response.send('fake logn register signup route')
+    const { username, password } = request.body
+
+    if(!username || !password) {
+        throw new BadRequestError('Please provide both email and password')
+    }
+
+    const id = new Date().getDate() // for demo onli
+
+    const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn: '30d'})
+    // console.log(username, password)
+    response.status(200).json({ msg: 'user created', token})
 }
 
-const dashboard = async (requet, response) => {
+const dashboard = async (request, response) => {
     const luckyNumber = Math.floor(Math.random() * 100)
-    response.status(200).json({ msg: `Hello, john doe`, secret: `here is your authorized data, your lucky number is ${luckyNumber}`})
+    response.status(200).json({ msg: `Hello, ${request.user.username}`, secret: `here is your authorized data, your lucky number is ${luckyNumber}`})
 }
 
 module.exports = {
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,9 +1,10 @@
-const CustomAPIError = require('../errors/custom-error')
+const {CustomAPIError} = require('../errors')
+const {StatusCode} = require('http-status-codes')
 const errorHandlerMiddleware = (error, request, response, next) => {
     if(error instanceof CustomAPIError) {
         return response.status(error.statusCode).json({ msg: error.message })
     }
-    return response.status(500).send('something went wrong try again later...')
+    return response.status(StatusCode.INTERNAL_SERVER_ERROR).send('Something went wrong try again later...')
 }
 
 module.exports = errorHandlerMiddleware
